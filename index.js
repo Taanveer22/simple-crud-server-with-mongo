c028b3b9fb00a2d36af6ddff1ac447f888ab14d9
Taanveer22 <taanveer469@gmail.com>
2026-02-19 07:28:00 +0600

    alhamdulliah dot env file added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // required items
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -11,11 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// =====================taanveer469_db_user===============
 // send data from server to database via mongodb
-// =====================hbjcd6XrTBVpiTRI==================
-const uri =
-  "mongodb+srv://taanveer469_db_user:hbjcd6XrTBVpiTRI@cluster0.89rnkti.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -92,7 +91,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
     // Ensures that the client will close when you finish/error
